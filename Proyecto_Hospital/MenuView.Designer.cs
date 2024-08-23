ba754c1f723dacdcbe73314354ae21d2970d5fbb
Saúl Maldonado lópez <Alejandrom199916@gmail.com>
2024-08-22 22:04:49 -0500

    Update: Primeros cambios

--- Proyecto_Hospital/MenuView.Designer.cs
+++ Proyecto_Hospital/MenuView.Designer.cs
@@ -129,7 +129,7 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(3, 4);
+            pictureBox1.Location = new Point(4, 4);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(200, 73);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
